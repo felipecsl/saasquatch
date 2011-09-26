b6b885a3b5444f507be40c976090c562bb61c084
Felipe Lima <felipe.lima@gmail.com>
2011-09-26 12:05:57 -0300

    Initial rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Saasquatch::Application.config.secret_token = '11a9fe9137df88e7d1be4d31cd66e7a9d1bc32922e57abc08cd80cce382e67122ee9a9ba3b6b9ef16ae239e58c0a26088055e52e4a238aa62d9d6d9aee65b49a'
